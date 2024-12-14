a86de3d3c2371e09a76ef4acec49e8307ffedf5e
SamPatatoz <issamissam@hotmail.be>
2024-12-14 20:26:36 +0100

    Implemented the fractal

--- fractal.asm
+++ fractal.asm
@@ -5,5 +5,61 @@
 |; @param sideLength  the length of the side of the current square.
 |; @param maxDepth    the maximum depth of the fractal.
 drawFractal:
+    PUSH(LP) PUSH(BP)
+    MOVE(SP, BP)
 
-    |; Place your code here
+    PUSH(R1) PUSH(R2) PUSH(R3)
+    PUSH(R4) PUSH(R5) PUSH(R6) PUSH(R7) PUSH(R8)
+
+    LD(BP, -12, R1)             |; xTopLeft
+    LD(BP, -16, R2)             |; yTopLeft
+    LD(BP, -20, R3)             |; sideLength
+    LD(BP, -24, R4)             |; maxDepth
+
+    |; Check if maxDepth == 0
+    CMOVE(0, R0)                |; R0 = 0
+    BEQ(R4, end_fractal, R0)    |; if maxDepth == 0, end the fractal
+
+    |; Check if sideLength == 0
+    ANDC(R3, 1, R0)             |; R0 = sideLength & 1
+    BEQ(R0, skip_adjust, R31)   |; if sideLength is even, skip the adjustment
+    SUBC(R3, 1, R3)             |; otherwise, sideLength--
+
+skip_adjust:
+
+    PUSH(R4)                    |; Push maxDepth
+    PUSH(R3) PUSH(R2) PUSH(R1)  |; Push sideLength, yTopLeft, xTopLeft
+    CALL(drawSquare, 3)         |; Draw the square
+    POP(R4) |; Restore maxDepth
+
+    |; Compute the center of the circle
+    DIVC(R3, 2, R5)             |; R5 = radius = sideLength / 2
+    ADD(R1, R5, R6)             |; R6 = xc = xTopLeft + radius
+    ADD(R2, R5, R7)             |; R7 = yc = yTopLeft + radius
+
+    PUSH(R5) PUSH(R7) PUSH(R6)  |; Push radius, yc, xc
+    CALL(drawCircleBres, 3)     |; Draw the circle
+    MOVE(R0, R8)                |; R8 = lastPixelCircleX (return the last pixel x-coordinate)
+
+    |; Compute the shift of the next square
+    SUB(R5, R8, R9)             |; R9 = shift = radius - lastPixelCircleX
+    CMOVE(1, R0)                |; R0 = 1
+    CMPLT(R0, R9, R0)           |; R0 = 1 si shift < 1
+    BEQ(R0, end_fractal, R31)   |; if shift < 1, end the fractal
+
+    |; Compute the new parameters for the next fractal
+    SUB(R3, R9, R3)             |; sideLength = sideLength - 2 * shift
+    SUB(R3, R9, R3)
+    ADD(R1, R9, R1)             |; xTopLeft = xTopLeft + shift
+    ADD(R2, R9, R2)             |; yTopLeft = yTopLeft + shift
+    SUBC(R4, 1, R4)             |; maxDepth--
+
+    PUSH(R4) PUSH(R3) PUSH(R2) PUSH(R1) |; Push maxDepth, sideLength, yTopLeft, xTopLeft
+    CALL(drawFractal, 4)        |; Draw the next fractal (recursive call)
+
+end_fractal:
+    POP(R8) POP(R7) POP(R6)
+    POP(R5) POP(R4) POP(R3) POP(R2) POP(R1)
+    POP(BP) POP(LP)
+    RTN()
+
--- square.asm
+++ square.asm
@@ -4,70 +4,71 @@
 |; @param yTopLeft    the y-coordinate of the top-left corner of the square.
 |; @param sideLength  the length of the side of the square (in number of pixels).
 drawSquare:
-    PUSH(LP) PUSH(BP)           |; Sauvegarder le pointeur de lien et le pointeur de base
-    MOVE(SP, BP)                |; Configurer le nouveau cadre de pile
+    PUSH(LP) PUSH(BP)
+    MOVE(SP, BP)
 
-    PUSH(R1) PUSH(R2) PUSH(R3)  |; Sauvegarder les registres nécessaires
+    PUSH(R1) PUSH(R2) PUSH(R3)
     PUSH(R4) PUSH(R5) PUSH(R6)
 
-    |; Charger les paramètres
     LD(BP, -12, R1)  |; xTopLeft
     LD(BP, -16, R2)  |; yTopLeft
     LD(BP, -20, R3)  |; sideLength
 
-    |; Calcul des coordonnées xBottomRight et yBottomRight
-    ADDC(R3, -1, R0)            |; R3 = sideLength - 1
+    |; Compute xBottomRight and yBottomRight
+    ADDC(R3, -1, R3)            |; R3 = sideLength - 1
     ADD(R1, R3, R4)             |; R4 = xBottomRight
     ADD(R2, R3, R5)             |; R5 = yBottomRight
-    |; Boucle pour yCur (R6)
+
     MOVE(R2, R6)                |; R6 = yTopLeft (yCur)
-y_loop:
-    CMPLE(R6, R5, R0)           |; Comparer yCur <= yBottomRight
-    BEQ(0, end_program, R31)   |; Sortir si yCur > yBottomRight
-    |; Boucle pour xCur (R7)
-    MOVE(R1, R7)                |; R7 = xTopLeft (xCur)
-x_loop:
-    CMPLE(R7, R4, R0)           |; Comparer xCur <= xBottomRight
-    BEQ(R0, next_y, R31)        |; Sortir si xCur > xBottomRight
 
-    |; Vérifier si le pixel est sur le bord
-    |; xCur == xTopLeft ou xCur == xBottomRight
-    SUB(R7, R1, R8)             |; R8 = xCur - xTopLeft
-    CMOVE(0, R9)                |; R9 = 0
-    BEQ(R8, is_on_edge, R9)     |; Si xCur == xTopLeft, pixel sur le bord
+    y_loop:
+        CMPLE(R6, R5, R0)           |; Is yCur <= yBottomRight?
+        BEQ(0, end_program, R31)    |; Exit if yCur > yBottomRight
 
-    SUB(R7, R4, R8)             |; R8 = xCur - xBottomRight
-    CMOVE(0, R9)
-    BEQ(R8, is_on_edge, R9)     |; Si xCur == xBottomRight, pixel sur le bord
+        MOVE(R1, R7)                |; R7 = xTopLeft (xCur)
 
-    |; yCur == yTopLeft ou yCur == yBottomRight
-    SUB(R6, R2, R8)             |; R8 = yCur - yTopLeft
-    CMOVE(0, R9)
-    BEQ(R8, is_on_edge, R9)     |; Si yCur == yTopLeft, pixel sur le bord
+    x_loop:
+        CMPLE(R7, R4, R0)           |; Is xCur <= xBottomRight?
+        BEQ(R0, next_y, R31)        |; Exit if xCur > xBottomRight
 
-    SUB(R6, R5, R8)             |; R8 = yCur - yBottomRight
-    CMOVE(0, R9)
-    BEQ(R8, is_on_edge, R9)     |; Si yCur == yBottomRight, pixel sur le bord
+        |; Check if the current pixel is on the edge of the square
+        |; xCur == xTopLeft or xCur == xBottomRight
+        SUB(R7, R1, R8)             |; R8 = xCur - xTopLeft
+        CMOVE(0, R9)                |; R9 = 0
+        BEQ(R8, is_on_edge, R9)     |; If xCur == xTopLeft, pixel is on the edge
 
-    BR(skip_pixel)             |; Si aucune condition n'est remplie, ignorer le pixel
+        SUB(R7, R4, R8)             |; R8 = xCur - xBottomRight
+        CMOVE(0, R9)
+        BEQ(R8, is_on_edge, R9)     |; If xCur == xBottomRight, pixel is on the edge
 
-is_on_edge:
-    |; Dessiner le pixel (xCur, yCur)
-    PUSH(R6) PUSH(R7)           |; Empiler yCur et xCur
-    CALL(canvas_set_to_1, 2)    |; Appeler canvas_set_to_1
+        |; yCur == yTopLeft or yCur == yBottomRight
+        SUB(R6, R2, R8)             |; R8 = yCur - yTopLeft
+        CMOVE(0, R9)
+        BEQ(R8, is_on_edge, R9)     |; If yCur == yTopLeft, pixel is on the edge
 
-skip_pixel:
-    ADDC(R7, 1, R7)             |; xCur++
-    BR(x_loop)                 |; Recommencer pour xCur
+        SUB(R6, R5, R8)             |; R8 = yCur - yBottomRight
+        CMOVE(0, R9)
+        BEQ(R8, is_on_edge, R9)     |; If yCur == yBottomRight, pixel is on the edge
 
-next_y:
-    ADDC(R6, 1, R6)             |; yCur++
-    BR(y_loop)                 |; Recommencer pour yCur
+        BR(skip_pixel)             |; Skip the pixel if it is not on the edge
 
-end_program:
-    POP(R6) POP(R5) POP(R4)     |; Restaurer les registres
-    POP(R3) POP(R2) POP(R1)
-    POP(BP) POP(LP)
-    RTN()                       |; Retour
+    is_on_edge:
+        |; Draw pixel (xCur, yCur)
+        PUSH(R6) PUSH(R7)           |; Push yCur, xCur
+        CALL(canvas_set_to_1, 2)    |; Set the pixel to 1
+
+    skip_pixel:
+        ADDC(R7, 1, R7)             |; xCur++
+        BR(x_loop)                  |; loop again for xCur
+
+    next_y:
+        ADDC(R6, 1, R6)             |; yCur++
+        BR(y_loop)                  |; loop again for yCur
+
+    end_program:
+        POP(R6) POP(R5) POP(R4)
+        POP(R3) POP(R2) POP(R1)
+        POP(BP) POP(LP)
+        RTN()
 
 
